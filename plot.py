449e3dbd8abc04c14ee8a0ec64ea8829ca0ed0bf
Andrea Gasparin <andrea.gasparin.87@gmail.com>
2019-12-21 21:53:25 +0100

    funzionante

--- plot.py
+++ plot.py
@@ -2,7 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mlt
-import data as dt
+#import data as dt
+import os
+
+
+
+
+
+
 
 
 df=pd.read_csv("dati/df_fai.csv", sep=",")
@@ -11,7 +18,7 @@ df.columns=[i if "/" not in i else i.replace("/","-") for i in df.columns]
 df.columns=[i if "€" not in i else i.replace("€","eur") for i in df.columns]
 df.columns=[i if "%" not in i else i.replace("%","perc") for i in df.columns]
 
-df.columns
+
 
 df.sort_values(by="abitanti",ascending=False)
 
@@ -19,15 +26,8 @@ df_abitanti=df.copy(deep=True)
 
 
 
-df_abitanti=df_abitanti[df_abitanti["abitanti"]<150000]
-df_abitanti.drop_duplicates("delegazione",inplace=True)
-df_abitanti.shape
-df_abitanti.sort_values(by="abitanti",ascending=False,inplace=True)
 mlt.rcParams["figure.figsize"]=(20,10)
 mlt.rcParams["font.size"]=20
-plt.plot(np.arange(df_abitanti.shape[0]),df_abitanti["abitanti"])
-plt.hist(df_abitanti["abitanti"],bins=20)
-plt.hist(df_abitanti["abitanti"],bins=30)
 
 
 df_piccoli=df[df["abitanti"]<20000]
@@ -44,41 +44,48 @@ df_enormi=df_enormi[df_enormi["abitanti"]<1000000]
 df_megalopoli=df[df["abitanti"]>=1000000]
 df_megalopoli.iloc[:20]
 
-np.unique(df["anno"].to_numpy())
+
+
+
 
 
 def migliori(df_local,folder):
-    %matplotlib agg
-    plt.ioff()
+    try:
+        os.mkdir("../plots/"+folder)
+    except OSError:
+        pass
+
     dividendi=["tot_entrate","totale_n+r","ctb_visitatori"]
     targets=["luoghi_aperti","visitatori","abitanti","densità","reddito medio"]
     periodi=["GFP","GFA"]
     for dividendo in dividendi:
+        try:
+            os.mkdir("../plots/"+folder+"/"+dividendo)
+        except OSError:
+            pass
         for periodo in periodi:
+            try:
+                os.mkdir("../plots/"+folder+"/"+dividendo+"/"+periodo)
+            except OSError:
+                pass
             for target in targets:
                 for anno in np.unique(df_local["anno"].to_numpy()):
+                    fig=plt.figure()
                     df_primavera=df_local[df_local["tipo"]==periodo]
                     df_primavera=df_primavera[df_primavera["anno"]==anno]
-                    df_primavera["target"]=df_primavera[dividendo].astype(float)/df_primavera[target]
+                    df_primavera["target"]=df_primavera[dividendo]/df_primavera[target]
                     df_primavera.sort_values(by="target",ascending=False,inplace=True)
                     df_primavera=df_primavera.iloc[:20]
                     plt.bar((df_primavera['delegazione'].values).astype(str),df_primavera["target"])
                     plt.title(target+"  "+str(anno)+"  "+periodo)
                     plt.xticks(rotation='vertical')
                     plt.gcf().subplots_adjust(bottom=0.50)
-                    plt.savefig("plots/"+folder+"/"+dividendo+"/"+dividendo+"-"+target+" "+str(anno)+" "+periodo+".png")
-                    plt.show()
+                    fig.savefig("../plots/"+folder+"/"+dividendo+"/"+periodo+"/"+target+" "+str(anno)+" "+periodo+".png")
+                    plt.close()
 
-
+os.mkdir("../plots")
 migliori(df_piccoli,"piccoli")
 migliori(df_medi,"medi")
 migliori(df_grandi,"grandi")
 migliori(df_enormi,"enormi")
 migliori(df_megalopoli,"megalopoli")
-
-
-for i in df_piccoli["ctb_visitatori"]:
-    print(i)
-
-df_piccoli["ctb_visitatori"]/df_piccoli["luoghi_aperti"]
-df_piccoli["ctb_visitatori"].astype(float)
