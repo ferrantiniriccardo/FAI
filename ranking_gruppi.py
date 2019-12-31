b8c8965a94ca1162980390448338438319074367
Andrea Gasparin <andrea.gasparin.87@gmail.com>
2019-12-31 15:31:24 +0100

    ranking gruppi ok

--- ranking_gruppi.py
+++ ranking_gruppi.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib as mlt
+import modello as md
 import os
 
 
@@ -11,17 +12,20 @@ import os
 
 df,little,middle_1,middle_2,middle_3,middle,big,huge,all_no_little,thresholds,thresholds_short=coo.make_all_df()
 
-def ranking_gruppi_media(item,lista_df,labels,df,save=False):
+def ranking_gruppi_media(item,k,n,lista_df,labels,df,save=False):
     if save==True:
         try:
             os.mkdir("../plots/"+str(item)+" ranking gruppi medie")
         except OSError:
             pass
     efficienza=np.array([])
-    colors=["black","red","blue","yellow","green"]
+    if len(labels)==4:
+        colors=np.array(["deepskyblue","orange","g","fuchsia"])
+    else:
+        colors=np.array(["deepskyblue","#f5b303","orange","darkorange","g","fuchsia"])
 
     for d in lista_df:
-        efficienza=np.append(efficienza,sum(d[item])/sum(d["abitanti"]))
+        efficienza=np.append(efficienza,sum(d[item]/(k*d["abitanti"])**n))
 
     sort_index=np.argsort(efficienza)
     efficienza=np.sort(efficienza)
@@ -35,28 +39,17 @@ def ranking_gruppi_media(item,lista_df,labels,df,save=False):
 
 
 
-lista_df=[middle_1,middle_2,middle_3,big,huge]
-labels=["medi 1","medi 2","medi 3","grandi", "metropoli"]
-ranking_gruppi_media("tot_entrate",lista_df,labels,df)
+k,n=md.fit_model()
+
+
+lista_df=[little,middle_1,middle_2,middle_3,big,huge]
+labels=["piccoli","medi 1","medi 2","medi 3","grandi", "metropoli"]
+ranking_gruppi_media("tot_entrate",k,n,lista_df,labels,df)
+ranking_gruppi_media("totale_n+r",k,n,lista_df,labels,df)
+
+
+
+lista_df=[little,middle,big,huge]
+labels=["piccoli","medi","grandi", "metropoli"]
+ranking_gruppi_media("tot_entrate",k,n,lista_df,labels,df)
 ranking_gruppi_media("totale_n+r",lista_df,labels,df)
-
-
-
-lista_df=[middle,big,huge]
-labels=["medi","grandi", "metropoli"]
-ranking_gruppi_media("tot_entrate",lista_df,labels,df)
-ranking_gruppi_media("totale_n+r",lista_df,labels,df)
-
-
-
-fun.cerca_delegazione("ancona",df)
-ancona=df[df["delegazione"]=="Delegazione Ancona"]
-ancona=ancona.append(df[df["delegazione"]=="Delegazione FAI Ancona"])
-ancona
-
-vimerc=fun.cerca_delegazione("mercatese",df)
-
-vi=df[df["delegazione"]==vimerc[0]]
-for v in vimerc[1:]:
-    vi=vi.append(df[df["delegazione"]==v])
-vi
